0e113bce80e49a510c7873fb4d19a7123b2df0d3
dimionik <dimionik952@gmail.com>
2026-05-17 12:38:29 +0300

    ЛР11

---
+++ TrainDispatcher/EditDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace TrainDispatcher
+{
+    public class EditDB
+    {
+        public int trainNum { get; set; }
+        public bool trainAdd { get; set; }
+
+        public EditDB()
+        {
+        }
+
+        public void ChangeDBRow()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(MainWindow.DataConnection.connStr))
+                using (MySqlCommand cmd = new MySqlCommand(
+                    "UPDATE rozklad SET train_number = ?, destination = ?, " +
+                    "departure_time = ?, travel_time = ?, tickets_available = ? WHERE id = ?", conn))
+                {
+                    cmd.Parameters.Add("@train_number", MySqlDbType.VarChar, 6).Value = MainWindow.editedTrain.train_number;
+                    cmd.Parameters.Add("@destination", MySqlDbType.VarChar, 25).Value = MainWindow.editedTrain.destination;
+                    cmd.Parameters.Add("@departure_time", MySqlDbType.Time).Value = MainWindow.editedTrain.departure_time;
+                    cmd.Parameters.Add("@travel_time", MySqlDbType.Time).Value = MainWindow.editedTrain.travel_time;
+                    cmd.Parameters.Add("@tickets_available", MySqlDbType.Int32, 4).Value = MainWindow.editedTrain.tickets_available;
+                    cmd.Parameters.Add("@id", MySqlDbType.Int32, 11).Value = MainWindow.editedTrain.id;
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + char.ConvertFromUtf32(13) +
+                    char.ConvertFromUtf32(13) + "Помилка з'єднання з БД",
+                    "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- TrainDispatcher/MainWindow.xaml.cs
+++ TrainDispatcher/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace TrainDispatcher
     {
         public static Authorization logedUser = new Authorization();
         public static DataAccess DataConnection;
+        public static Train editedTrain;
+        public static EditDB editedRow = new EditDB();
 
         public MainWindow()
         {
@@ -29,6 +31,8 @@ namespace TrainDispatcher
         {
             TrainsMenuItem.Visibility = Visibility.Hidden;
             TrainsMenuItem.Width = 0;
+
+            trainGroupBox.Visibility = Visibility.Hidden;
 
             DataConnection = new DataAccess();
             TrainListDG.ItemsSource = DataConnection.fList;
@@ -71,6 +75,90 @@ namespace TrainDispatcher
 
             DataConnection = new DataAccess();
             TrainListDG.ItemsSource = DataConnection.fList;
+        }
+
+        private void EditDataMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            trainGroupBox.Visibility = Visibility.Visible;
+            TrainListDG.SelectedIndex = -1;
+            editedTrain = null;
+
+            MessageBox.Show("Оберіть у списку запис для редагування подвійним кліком",
+                            "Увага!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
+        private void TrainListDG_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            editedTrain = TrainListDG.SelectedItem as Train;
+            try
+            {
+                numTrainTextBox.Text = editedTrain.train_number;
+                destTrainTextBox.Text = editedTrain.destination;
+                depTimeTextBox.Text = editedTrain.departure_time.ToString(@"hh\:mm");
+                travTimeTextBox.Text = editedTrain.travel_time.ToString(@"hh\:mm");
+                ticketsTextBox.Text = editedTrain.tickets_available.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            editedRow = new EditDB();
+            editedRow.trainNum = TrainListDG.SelectedIndex;
+            editedRow.trainAdd = false;
+        }
+
+        private void ChangeTrainListData(int num)
+        {
+            TimeSpan depTime;
+            TimeSpan travTime;
+            int tickets;
+
+            editedTrain.train_number = numTrainTextBox.Text;
+            editedTrain.destination = destTrainTextBox.Text;
+
+            if (!TimeSpan.TryParse(depTimeTextBox.Text, out depTime))
+            {
+                MessageBox.Show("Невірний формат часу відправлення. Використовуйте формат гг:хх",
+                                "Помилка!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            editedTrain.departure_time = depTime;
+
+            if (!TimeSpan.TryParse(travTimeTextBox.Text, out travTime))
+            {
+                MessageBox.Show("Невірний формат часу у дорозі. Використовуйте формат гг:хх",
+                                "Помилка!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            editedTrain.travel_time = travTime;
+
+            if (!int.TryParse(ticketsTextBox.Text, out tickets))
+            {
+                MessageBox.Show("Невірний формат кількості квитків. Введіть ціле число",
+                                "Помилка!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            editedTrain.tickets_available = tickets;
+
+            DataConnection.fList[num] = editedTrain;
+
+            TrainListDG.ItemsSource = null;
+            TrainListDG.ItemsSource = DataConnection.fList;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (editedTrain == null || TrainListDG.SelectedIndex < 0)
+            {
+                MessageBox.Show("Оберіть у списку запис для редагування подвійним кліком",
+                                "Увага!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                ChangeTrainListData(TrainListDG.SelectedIndex);
+                editedRow.ChangeDBRow();
+            }
         }
     }
 }
